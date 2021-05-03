d3a9ebafb9450ed376bae0616cdd7711d8ea0e09
Gusstrik <i.strelnikov73@gmail.com>
2021-05-03 16:56:20 +0300

    Changed DB schema

--- docliib-repository/src/main/resources/createdb.sql
+++ docliib-repository/src/main/resources/createdb.sql
@@ -1,66 +1,49 @@
-CREATE TABLE IF NOT EXISTS catalog
+CREATE TABLE IF NOT EXISTS catalogs
 (
     id     serial,
     name   varchar(20) not null unique,
     parent varchar(20),
     PRIMARY KEY (id)
 );
-CREATE TABLE IF NOT EXISTS types
+
+CREATE TABLE IF NOT EXISTS doc_types
 (
     id   serial,
-    type varchar(30) not null unique,
+    name varchar(30) not null unique,
     PRIMARY KEY (id)
 );
-CREATE TABLE IF NOT EXISTS clients
-(
-    id       serial,
-    login    varchar(30) not null unique,
-    password varchar(30) not null,
-    email    varchar(50) not null unique,
-    PRIMARY KEY (id)
-);
-CREATE TABLE IF NOT EXISTS document
-(
-    id           serial          not null,
-    name         varchar(20)     not null,
-    type         varchar(20)     not null,
-    version      int default (0) not null,
-    description  varchar(200),
-    catalog      int,
-    is_moderated bool,
+
+CREATE TABLE IF NOT EXISTS documents(
+    id serial not null ,
+    name varchar(20) not null,
+    type varchar(20) not null ,
+    actual_version int default (0) not null,
+    catalog_id int not null,
     PRIMARY KEY (id),
-    FOREIGN KEY (type) references types (type) on update cascade on delete cascade,
-    FOREIGN KEY (catalog) references catalog (id) on update cascade on delete cascade
+    FOREIGN KEY (type) references doc_types(name) on update  cascade on delete cascade,
+    FOREIGN KEY (catalog_id) references catalogs(id) on update cascade on  delete cascade
 );
-CREATE TABLE IF NOT EXISTS file
+CREATE TABLE IF NOT EXISTS  documents_versions(
+    id serial not null,
+    document_id int not null,
+    version int not null,
+    description varchar(300),
+    importance varchar(10) not null,
+    is_moderated bool default false not null,
+    PRIMARY KEY (id),
+    FOREIGN KEY  (document_id) references documents(id) on update cascade on delete cascade
+);
+CREATE TABLE IF NOT EXISTS doc_files
 (
     id          serial,
     name        varchar(20)  not null,
     document_id int          not null,
     path        varchar(300) not null,
     PRIMARY KEY (id),
-    FOREIGN KEY (document_id) references document (id) on update cascade on delete cascade
+    FOREIGN KEY (document_id) references documents_versions (id) on update cascade on delete cascade
 );
-create table if not exists catalog_client
-(
-    catalog_id int not null,
-    client_id  int not null,
-    permission int default 1,
-    foreign key (catalog_id) references catalog (id) on update cascade on delete cascade,
-    foreign key (client_id) references clients (id) on update cascade on delete cascade
-);
-create table if not exists document_client
-(
-    document_id int         not null,
-    client_id   int not null,
-    permission  int default 1,
-    foreign key (document_id) references document (id) on update cascade on delete cascade,
-    foreign key (client_id) references clients (id) on update cascade on delete cascade
-);
-
-
-INSERT INTO catalog(id, name)
-    SELECT nextval('catalog_id_seq'), '/'
+INSERT INTO catalogs(id, name)
+    SELECT 1, '/'
 WHERE NOT EXISTS (
-    SELECT 1 FROM catalog WHERE name='/'
+    SELECT 1 FROM catalogs WHERE name='/'
 );
