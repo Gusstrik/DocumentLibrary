c21c6ea4e7fb21b6508fccc9426119cb15c6eb18
Gusstrik <i.strelnikov73@gmail.com>
2021-05-11 10:47:34 +0300

    Added DocumentRestController

--- doclib-web/src/main/java/com/strelnikov/doclib/web/controllers/DocumentRestController.java
+++ doclib-web/src/main/java/com/strelnikov/doclib/web/controllers/DocumentRestController.java
@@ -2,7 +2,9 @@ package com.strelnikov.doclib.web.controllers;
 
 import com.strelnikov.doclib.dto.DocumentDto;
 import com.strelnikov.doclib.service.DocumentActions;
+import com.strelnikov.doclib.service.exceptions.UnitIsAlreadyExistException;
 import com.strelnikov.doclib.service.exceptions.UnitNotFoundException;
+import com.strelnikov.doclib.service.exceptions.VersionIsAlreadyExistException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,11 +17,34 @@ public class DocumentRestController {
     DocumentActions docAct;
 
     @GetMapping("{id}")
-    public ResponseEntity<DocumentDto> getDocuemnt(@PathVariable int id){
+    public ResponseEntity<DocumentDto> getDocument(@PathVariable int id){
         try{
             DocumentDto docDto = docAct.loadDocument(id);
             return ResponseEntity.ok(docDto);
         } catch (UnitNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> postDocument(@RequestBody DocumentDto documentDto){
+        try {
+            documentDto = docAct.saveDocument(documentDto);
+            return ResponseEntity.ok(documentDto);
+        } catch (VersionIsAlreadyExistException e) {
+            return ResponseEntity.badRequest().body("Version " + e.getDocVer().getVersion()+ " of document is already exist");
+        } catch (UnitIsAlreadyExistException e) {
+            return ResponseEntity.badRequest().body("Document is already exist in catalog");
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Object> deleteDocument(@PathVariable int id){
+        try {
+            DocumentDto documentDto = docAct.loadDocument(id);
+            docAct.deleteDocument(id);
+            return ResponseEntity.ok("Document was successfully deleted");
+        } catch (UnitNotFoundException e) {
             return ResponseEntity.notFound().build();
         }
     }
